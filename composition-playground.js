9081c83accb871e63c8db651ea5db97b07e8e60c
Ulric Wilfred <shaman.sir@gmail.com>
2012-09-12 18:57:26 +0400

    dynamic example

--- composition-playground.js
+++ composition-playground.js
@@ -29,12 +29,22 @@ var examples = [
   function(ctx, t) {
     ctx.fillStyle = '#f00';
     ctx.fillRect(50, 50, 100, 100);
+  },
+  function(ctx, t) {
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(100, 100, 100, 100);
+  },
+  function(ctx, t) {
+    ctx.fillStyle = '#00f';
+    ctx.fillRect(t/10*300, 0, 200, 200);
   }
 ];
 
 var workspace,
     resultCvs,
     resultCtx;
+
+var __lock = false;
 
 var __nextFrame = (function() {
    return window.requestAnimationFrame ||
@@ -83,6 +93,7 @@ function addCanvas() {
 
     opElm.onclick = /*(function(data, iconElm, textElm) {
         return */function() {
+            __lock = true;
             if (data.op < (compositions.length - 1)) {
                data.op++;
             } else {
@@ -91,10 +102,25 @@ function addCanvas() {
             var opName = compositions[data.op];
             iconElm.className = 'icon-'+icons[opName];
             textElm.innerHTML = opName;
+            __lock = false;
         };
-    /*})(data, iconElm, textElm)*/;
-  }
+    /*})(data, iconElm, textElm);*/
+  };
   workspace.appendChild(cvs);
+
+  cvs.onclick = /*(function(data) {
+      return */function() {
+          __lock = true;
+          data.ctx.clearRect(0, 0,
+               data.cvs.width, data.cvs.height);
+          if (data.example < (examples.length - 1)) {
+              data.example++;
+          } else {
+              data.example = 0;
+          }
+          __lock = false;
+      /*})(data, iconElm, textElm)*/;
+  };
 
   // add to DOM: "after" elm
   // TODO
@@ -103,13 +129,27 @@ function addCanvas() {
 function deleteLastCanvas() {
 }
 
-function compute() {
-  resultCtx.clearRect(0, 0, resultCvs.width, resultCvs.width);
+var startT = -1,
+    t = -1;
+var maxT = 10;
 
-  for (var i = 0, fl = formula.length; i < fl; i++) {
-    var data = formula[i];
-    examples[data.example](data.ctx);
-    resultCtx.drawImage(data.cvs, 0, 0);
+function compute() {
+  if (!__lock) {
+
+    if (t > maxT) startT = -1;
+    if (startT === -1) startT = new Date();
+    t = ((new Date()) - startT) / 1000;
+
+    resultCtx.clearRect(0, 0, resultCvs.width, resultCvs.width);
+
+    for (var i = 0, fl = formula.length; i < fl; i++) {
+      var data = formula[i];
+      data.ctx.clearRect(0, 0, data.cvs.width, data.cvs.height);
+      examples[data.example](data.ctx, t);
+      resultCtx.globalCompositeOperation = compositions[data.op];
+      resultCtx.drawImage(data.cvs, 0, 0);
+    }
+
   }
 
   __nextFrame(compute);
