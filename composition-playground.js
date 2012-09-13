07951f6ff6fc386cec5a05e9dee1b1008b2fcdc3
Ulric Wilfred <shaman.sir@gmail.com>
2012-09-13 14:18:42 +0400

    much more cool examples

--- composition-playground.js
+++ composition-playground.js
@@ -11,6 +11,7 @@ var compositions = [
   'lighter',
   'darker',
   'xor',
+  /*'copy',*/
   'destination-over',
   'destination-atop',
   'destination-in',
@@ -24,6 +25,7 @@ var icons = {
   'lighter': 'star-empty',
   'darker': 'star',
   'xor': 'random',
+  /*'copy': 'copy',*/
   'destination-over': 'remove-circle',
   'destination-atop': 'remove-sign',
   'destination-in': 'arrow-left',
@@ -31,13 +33,14 @@ var icons = {
 };
 var default_op = 0; /* source-over */
 
+var __image;
 var examples = [
   function(ctx, t) {
-    ctx.fillStyle = '#f00';
+    ctx.fillStyle = '#00f';
     ctx.fillRect(50, 50, 60, 60);
   },
   function(ctx, t) {
-    ctx.fillStyle = '#00f';
+    ctx.fillStyle = '#f00';
     ctx.beginPath();
     ctx.arc(110, 110, 30, 0, 2 * Math.PI, false);
     ctx.fill();
@@ -46,15 +49,61 @@ var examples = [
   },
   function(ctx, t) {
     ctx.fillStyle = '#090';
-    ctx.fillRect(t/period*250, 0, 50, 200);
+    ctx.fillRect(t/period*width, 0, 50, 200);
   },
   function(ctx, t) {
     ctx.beginPath();
     ctx.fillStyle = '#f00';
-    ctx.arc(t/(period/2)*250, 80, 50, 0, 2 * Math.PI, false);
+    ctx.arc(20, t/period*height, 30, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = '#0f0';
+    ctx.arc(70, height-(t/period*height), 10, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = '#00f';
+    ctx.arc(140, t/period*height, 20, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = '#ff0';
+    ctx.arc(175, height-(t/period*height), 40, 0, 2 * Math.PI, false);
     ctx.fill();
   },
   function(ctx, t) {
+    // image from: http://www.techrepublic.com/blog/geekend/build-a-real-bender-robot-that-brews-beer-talks/1022
+    if (!__image) {
+      __image = new Image();
+
+      __image.onload = function() {
+        ctx.drawImage(__image, 0, 0);
+      };
+      __image.src = "./bender_done.jpg";
+    }
+    ctx.drawImage(__image, 0, 0, width, height);
+  },
+  function(ctx, t) {
+    ctx.beginPath();
+    ctx.fillStyle = '#000';
+    ctx.arc(t/(period/2)*width, 80, 50, 0, 2 * Math.PI, false);
+    ctx.fill();
+  },
+  function(ctx, t) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0,0,0,.5)';
+    ctx.arc(t/(period/2)*width, 80, 50, 0, 2 * Math.PI, false);
+    ctx.fill();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,width,height);
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,255,255,.5)';
+    ctx.arc(t/(period/2)*width, 80, 50, 0, 2 * Math.PI, false);
+    ctx.fill();
+  },
+  function(ctx, t) {
+    // from: http://www.html5canvastutorials.com/advanced/html5-canvas-clipping-region-tutorial/
+
     var x = width / 2;
     var y = height / 2;
     var radius = 70;
@@ -100,6 +149,58 @@ var examples = [
     ctx.stroke();
   },
   function(ctx, t) {
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle= "black";
+    ctx.stroke();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle= "#fff";
+    ctx.stroke();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.fillStyle= "#fff";
+    ctx.fill();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.fillStyle= "#000";
+    ctx.fill();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = 'rgba(1,1,1,0)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'rgba(1,1,1,.5)';
+    ctx.fill();
+  },
+  function(ctx, t) {
+    ctx.fillStyle = 'rgba(1,1,1,.3)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.arc(100, 75, 70, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.fillStyle = 'rgba(1,1,1,.6)';
+    ctx.fill();
+  },
+  function(ctx, t) {
     var grad = ctx.createLinearGradient(0,0,width,height);
     grad.addColorStop(t/period, '#fff');
     grad.addColorStop(1, '#000');
@@ -116,6 +217,55 @@ var examples = [
     ctx.font = '8pt sans-serif';
     ctx.fillStyle = '000';
     ctx.fillText("White is transparent!", 10, 20);
+  },
+  function(ctx, t) {
+    // from: https://developer.mozilla.org/en-US/docs/Canvas_tutorial/Compositing
+
+    var w = width, h = height,
+        wh = w/2, hh = h/2;
+
+    function drawStar(ctx,r){
+      ctx.save();
+      ctx.beginPath()
+      ctx.moveTo(r,0);
+      for (var i=0;i<9;i++){
+        ctx.rotate(Math.PI/5);
+        if(i%2 == 0) {
+          ctx.lineTo((r/0.525731)*0.200811,0);
+        } else {
+          ctx.lineTo(r,0);
+        }
+      }
+      ctx.closePath();
+      ctx.fill();
+      ctx.restore();
+    }
+
+    ctx.fillRect(0,0,w,h);
+    ctx.translate(wh,hh);
+
+    // Create a circular clipping path
+    ctx.beginPath();
+    ctx.arc(0,0,65,0,Math.PI*2,true);
+    ctx.clip();
+
+    // draw background
+    var lingrad = ctx.createLinearGradient(0,-wh,0,hh);
+    lingrad.addColorStop(0, '#232256');
+    lingrad.addColorStop(1, '#143778');
+
+    ctx.fillStyle = lingrad;
+    ctx.fillRect(-wh,-hh,w,h);
+
+    // draw stars
+    for (var j=1;j<50;j++){
+      ctx.save();
+      ctx.fillStyle = '#fff';
+      ctx.translate(wh-Math.floor(Math.random()*w),
+                    hh-Math.floor(Math.random()*h));
+      drawStar(ctx,Math.floor(Math.random()*4)+2);
+      ctx.restore();
+    }
   }
 ];
 
